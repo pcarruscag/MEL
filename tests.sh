e475db385c31f0d37daf594eb1ee8725b054fa5f
Pedro Gomes <pcarruscag@gmail.com>
2021-12-27 19:52:32 +0000

    add test script

---
+++ tests.sh
@@ -0,0 +1,4 @@
+rm tests.o tests
+g++ -pedantic -Wextra -Wall -Wconversion -Werror -std=c++11 -m64 -O2 -march=native -funroll-loops -ftree-vectorize -DNDEBUG -c ./tests.cpp -o ./tests.o
+g++ -o tests ./tests.o -m64 -s
+./tests
