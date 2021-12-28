be54de37a206e0b17cd8d06e7f993ae598f94257
Pedro Gomes <pcarruscag@gmail.com>
2021-12-28 11:42:56 +0000

    separate building and running tests

--- build_tests.sh
+++ build_tests.sh
@@ -1,4 +1,3 @@
 rm tests.o tests
 g++ -pedantic -Wextra -Wall -Wconversion -Werror -std=c++11 -m64 -O2 -march=native -funroll-loops -ftree-vectorize -DNDEBUG -c ./tests.cpp -o ./tests.o
 g++ -o tests ./tests.o -m64 -s
-./tests
